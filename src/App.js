a4b63ed1b6ed63ccd81504974e7aec26c37680c5
Andrei <ciobanca.andreisorin@gmail.com>
2024-08-09 14:23:26 +0300

    fixed route

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Container from "./components/Container/Container";
 import Menu from "./components/Menu/Menu";
 import QuestionType from "./components/QuestionType/QuestionType";
 import QuestionDetails from "./components/QuestionType/QuestionDetails/QuestionDetails";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <Container>
-      <Routes>
-        <Route path="/Exam-practice-app" element={<Menu />} />
-        <Route path="/Exam-practice-app/:year" element={<QuestionType />}>
-          <Route path=":type" element={<QuestionDetails />} />
-        </Route>
-        <Route path="*" element={<Menu />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Exam-practice-app" element={<Menu />} />
+          <Route path="/Exam-practice-app/:year" element={<QuestionType />}>
+            <Route path=":type" element={<QuestionDetails />} />
+          </Route>
+          <Route path="*" element={<Menu />} />
+        </Routes>
+      </BrowserRouter>{" "}
     </Container>
   );
 }
